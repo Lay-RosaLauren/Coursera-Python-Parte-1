a8ba8d4a90f7820f87280f0f4d8ad9f737ea1b93
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-23 16:18:55 -0300

    Semana02-Exercícios

---
+++ Week02/médias.py
@@ -0,0 +1,14 @@
+print("Cálculo de Média Aritmética", "\n")
+      
+# Entrada de dados:
+
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+nota3 = float(input("Digite a terceira nota:"))
+nota4 = float(input("Digite a quarta nota:"))
+
+# Saída de Dados
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print("A média aritmética é", media)
