6206e22499729e7012285fb34e949459b290f5e7
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-23 15:31:05 -0300

    Semana02-Exercícios

---
+++ Week02/fatura.py
@@ -0,0 +1,5 @@
+nome = input('Digite o nome do cliente: ')
+dia_vencimento = input('Digite o dia de vencimento: ')
+mes_vencimento = input('Digite o mês de vencimento: ')
+valor_fatura = input('Digite o valor da fatura: ')
+print("Olá, {}\nA sua fatura com vencimento em {} de {} no valor de R$ {} está fechada.".format(nome, dia_vencimento, mes_vencimento, valor_fatura))
