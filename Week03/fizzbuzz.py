1ad52391ada261df98c2375c7de3fc4a5ca7986a
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-24 16:28:54 -0300

    Semana03-Exercícios

---
+++ Week03/fizzbuzz.py
@@ -0,0 +1,9 @@
+número=int(input('Digite um número inteiro: '))
+
+if número%3==0 and número%5==0:
+
+    print('FizzBuzz')
+
+else:
+
+    print(número)
---
+++ Week03/paridade.py
@@ -0,0 +1,7 @@
+numero = float(input('Digite um número inteiro: '))
+resto = numero % 2
+
+if resto == 0:
+    print('par')
+else:
+    print('impar')
