276eb7072f63855e909c18fd3b3d2f99eb0dbfbe
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-23 21:53:51 -0300

    Semana02-Exercícios

---
+++ Week02/Semana02-Exercícios/quadrado.py
@@ -0,0 +1,11 @@
+print("Cálculo de Perímetro e Área de um quadrado:", "\n")
+
+#Entrada de Dados
+lado=a = int(input("Digite o valor correspondente ao lado de um quadrado:"))
+
+x=lado *4
+
+y=lado * lado
+
+#Saída de Dados
+print("perímetro:",x,"-","área:",y)
