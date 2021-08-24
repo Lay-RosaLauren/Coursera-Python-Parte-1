0abad770c4b8b74653e07f2bb76bb87531a2c022
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-24 13:20:00 -0300

    Delete ConversordeTemperatura.py

--- Week02/ConversordeTemperatura.py
+++
@@ -1,5 +0,0 @@
-temperaturaFahrenheit = input("Digite uma temperatura em Fahrenheit: ")
-
-temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5/9
-
-print("A temperatura em celsius é ", temperaturaCelsius)
