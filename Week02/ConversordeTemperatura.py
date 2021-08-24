ec09d12d1699582e0c6803464c39cf4589b30aca
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-24 13:08:28 -0300

    Week02

---
+++ Week02/ConversordeTemperatura.py
@@ -0,0 +1,5 @@
+temperaturaFahrenheit = input("Digite uma temperatura em Fahrenheit: ")
+
+temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5/9
+
+print("A temperatura em celsius é ", temperaturaCelsius)
