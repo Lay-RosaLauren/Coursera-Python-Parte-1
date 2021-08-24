14fbb7d0b1885ebf2e5be3f0b13a50ae303cd6e8
Rosa Lauren Barreira Nuevo de Campos <86569498+Lay-RosaLauren@users.noreply.github.com>
2021-08-24 15:38:28 -0300

    Semana01-Exercícios

---
+++ Week01/exercício3.py
@@ -0,0 +1,16 @@
+#Exercício 3 
+
+n = int(input("Digite um número inteiro: "))
+
+cont = 2
+ehNroPrimo = True
+
+while (cont < n and ehNroPrimo):
+
+    ehNroPrimo = not ((n % cont) == 0)
+    cont += 1
+
+if (ehNroPrimo):
+    print("primo")
+else:
+    print("não primo") 
